fb286c193faf8301378e9d331478772108997aee
Duany <duany@email.com>
2025-04-13 18:03:36 +0000

    Setup de monitoramento

--- todo_project/run.py
+++ todo_project/run.py
@@ -1,4 +1,8 @@
 from todo_project import app
+from prometheus_flask_exporter import PrometheusMetrics
+
+# Inicializa o monitoramento
+metrics = PrometheusMetrics(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
